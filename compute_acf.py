fbc3fae428ec10f9edea5b45de4e55cad14ccfe3
SMITHSNET\CDSFSSkatter <sondre.skatter@smiths.com>
2022-12-07 12:03:57 -0800

    FInished the temporal ACF

--- compute_acf.py
+++ compute_acf.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 
+use_log = True
+
 def calculate_spherical_distance(lat1, lon1, lat2, lon2, r=6371000):
     # Convert degrees to radians
     phi1, lambda1, phi2, lambda2 = [c*np.pi/180 for c in (lat1, lon1, lat2, lon2)]      
@@ -28,11 +30,20 @@ spores = df['spores'].values.reshape(N,1)
 #Collection_start_date = df['Collection_start_date'].values
 
 days_sampled = df['days_sampled'].values.reshape(N,1)
+spores /= days_sampled
+if use_log:
+    spores = np.log(spores+1)
 end_day = df['j_date'].values.reshape(N,1)
 beg_day = end_day - days_sampled
 
 center_date = end_day - 0.5 * days_sampled
 days_offset = np.abs(center_date - center_date.T)
+
+max_day_offset = np.maximum(np.maximum(np.abs(end_day - end_day.T),np.abs(end_day - beg_day.T)),
+    np.maximum(np.abs(beg_day - end_day.T), np.abs(beg_day - beg_day.T))).astype(np.int32).flatten()
+min_day_offset = np.minimum(np.minimum(np.abs(end_day - end_day.T),np.abs(end_day - beg_day.T)),
+    np.minimum(np.abs(beg_day - end_day.T), np.abs(beg_day - beg_day.T))).astype(np.int32).flatten()
+
 # do an intersect over union (IoU) for time overlap
 time_overlap = (np.maximum(0, np.minimum(end_day, end_day.T) - np.maximum(beg_day, beg_day.T)) / \
     (np.maximum(end_day, end_day.T) - np.minimum(beg_day, beg_day.T))).flatten()
@@ -49,21 +60,42 @@ y2 = (np.ones((N,1)) * np.square(spores.T)).flatten()
 same_sensor = np.where(((sensor_id == sensor_id.T) * (days_offset>0)).flatten())[0]
 days_offset = days_offset.flatten()
 
-time_bin_sz_days = 4
-d_offset = (np.round(days_offset[same_sensor] / time_bin_sz_days) * time_bin_sz_days).astype(np.int32)
-un_days, inds = np.unique(d_offset, return_inverse=True)
+if 1:
+    M = max_day_offset.max() + 1
+    xy_sum = np.zeros(M)
+    Ndatapts = np.zeros(M)
+    x2_sum = 1E-10*np.ones(M) 
+    y2_sum = 1E-10*np.ones(M) 
+    for i in same_sensor:
+        inds = np.arange(min_day_offset[i],max_day_offset[i]+1)
+        xy_sum[inds] += xy[i]
+        x2_sum[inds] += x2[i]
+        y2_sum[inds] += y2[i]
+        Ndatapts[inds] += 1
 
-ACF_time = np.zeros(un_days.size)
-Ndatapts = np.zeros(un_days.size)
-for i in range(un_days.size):
-    inds2 = np.where(inds == i)[0]
-    ACF_time[i] = np.sum(xy[same_sensor[inds2]]) / np.sqrt(np.sum(x2[same_sensor[inds2]])*np.sum(y2[same_sensor[inds2]]))
-    Ndatapts[i] = inds2.size
+    ACF_time = xy_sum / np.sqrt(x2_sum * y2_sum)
+    un_days = np.arange(M)
+else:
+    time_bin_sz_days = 4
+    d_offset = (np.round(days_offset[same_sensor] / time_bin_sz_days) * time_bin_sz_days).astype(np.int32)
+    un_days, inds = np.unique(d_offset, return_inverse=True)
+
+    ACF_time = np.zeros(un_days.size)
+    Ndatapts = np.zeros(un_days.size)
+    for i in range(un_days.size):
+        inds2 = np.where(inds == i)[0]
+        ACF_time[i] = np.sum(xy[same_sensor[inds2]]) / np.sqrt(np.sum(x2[same_sensor[inds2]])*np.sum(y2[same_sensor[inds2]]))
+        Ndatapts[i] = inds2.size
 
 fig, ax = plt.subplots()
 plt.plot(un_days,ACF_time)
-ax2 = ax.twinx()
-plt.plot(un_days, Ndatapts,'r')
+if 0:
+    ax2 = ax.twinx()
+    plt.plot(un_days, Ndatapts,'r')
+ax.set_xlim(left=0,right=150 if use_log else 50)
+ax.set_ylim(bottom=0)
+ax.set_xlabel('#Days apart for measurements on the same sensor')
+ax.set_ylabel(f'Auto correlation {"log([spore count] + 1)" if use_log else "spore count"}')
 plt.show()
 d = 1
 
